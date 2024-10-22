0005eff837a1d10e1619c402dfc24d4b3b0fa557
Aharbtech <9951@holbertonstudents.com>
2024-10-22 12:10:51 +0000

    Hello, printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Prints String
- * Return: 0
+ * main - Prints with newline
+ * Return: Description of the returned value
  **/
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+		return (0);
 }
