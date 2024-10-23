e88a0fc399e482e93e9c5de47ede6a7504efde1a
Aharbtech <9951@holbertonstudents.com>
2024-10-23 11:59:24 +0200

    Print Remainder Digit and compare

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	m = n % 10;
 
-	printf("Last digit of %d is", n);
+	printf("Last digit of %d is ", n);
 	if (m > 5)
 	{
 		printf("%d and is greater than 5\n", m);
