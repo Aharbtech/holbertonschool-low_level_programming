0fdb29b095eeed8ab517214925e23550ef6ccf7d
Aharbtech <9951@holbertonstudents.com>
2024-10-23 11:18:10 +0200

    Print Remainder Digit and compare

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -20,15 +20,15 @@ int main(void)
 	printf("Last digit of %d is ", n);
 	if (m > 5)
 	{
-		printf("%d, and is greater than 5 \n", m);
+		printf("%d and is greater than 5 \n", m);
 	}
 	else if (m == 0)
 	{
-		printf("%d, and is 0 \n", m);
+		printf("%d and is 0 \n", m);
 	}
 	else if (m < 6 && m != 0)
 	{
-		printf("%d, and is less than 6 and not 0 \n", m);
+		printf("%d and is less than 6 and not 0 \n", m);
 	}
 	return (0);
 }
