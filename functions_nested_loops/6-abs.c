2bd798d0792752b499a79fc5573d7239703729c5
Aharbtech <9951@holbertonstudents.com>
2024-11-05 12:42:11 +0000

    absolute number generator

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - Computes absolute value of int
+ * @int: integer to input
+ * Return: absolute value of int
+ **/
+
+int _abs(int n)
+{
+	if (n >=  0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		n = n * -1;
+		return (n);
+	}
+	return (0);
+}
