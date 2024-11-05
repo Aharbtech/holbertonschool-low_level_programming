4bbe341d8253988dadbd85cabee7d75ac0af3bfa
Aharbtech <9951@holbertonstudents.com>
2024-11-05 15:07:23 +0000

    counter to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,11 +12,16 @@ void print_to_98(int n)
 	{
 		for (; n < 98; n++)
 			printf("%d, ", n);
+		printf("%d \n", 98);
 	}
 	else if (n > 98)
 	{
 		for (; n > 98; n--)
 			printf("%d, ", n);
+		printf("%d \n", 98);
 	}
-	printf("%d \n", 98);
+	else if (n == 98)
+	{
+		printf("%d \n", 98);
+	}
 }
