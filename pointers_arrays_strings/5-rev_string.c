fd1fd44bfb8d52f1f17add80b238183d42107dae
Aharbtech <9951@holbertonstudents.com>
2024-11-12 11:50:59 +0000

    Reverse String

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,15 @@
 #include "main.h"
 
-void rev_string(char *s);
+void rev_string(char *s)
 {
-	int len = _strlen(char *s) - 1;
-	int char *c[_strlen(char *s)];
-
-	for (; len >= 0; len--)
-		*s[] = *c[len]
-		s++;
+	int len = _strlen(s) - 1;
+	char *c;
+	int i = 0;
+	
+	while (len >= 0)
+	{
+		c[i] = s[len];
+		i++;
+		len--;
+	}
 }
