fc4e1e91d7b1d74755a67b53dcdc54f98e66f871
Aharbtech <9951@holbertonstudents.com>
2024-11-14 17:24:45 +0000

    Strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * *_strcpy - Copies a String to a Buffer
+ * @dest: Destination
+ * @src: Source
+ * Return: Pointer to Dest
+ **/
+
+char *_strcpy(char *dest, char *src)
+{
+	
+}
