3d07612c84893d324aea09305809c10d3701d379
Aharbtech <9951@holbertonstudents.com>
2024-11-14 16:17:40 +0000

    Print Second Half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -2,30 +2,37 @@
 
 void puts_half(char *str)
 {
-	int length_of_the_string = _strlen(str);
-	
-	if (length_of_the_string % 2 == 0)
-	{	
-		int n = length_of_the_string / 2;
-		
-		while (*str != '\0')
-		{	_putchar(str[n]);
-			n++;
-		}
-	}
-	
-	else if (length_of_the_string % 2 != 0 && length_of_the_string != 1)
-	{	
-		int n = (length_of_the_string - 1) / 2;
-		
-		while (*str != '\0')
-		{	_putchar(str[n]);
-			n++;
-		}
-	}
-	else if (length_of_the_string == 1)
+	int length_of_the_string = 0;
+	int n;
+	char *str1 = str;
+
+	while (*str1 != '\0')
 	{
-		_putchar(str[0]);
+		str1++;
+		length_of_the_string++;
 	}
+
+	if (length_of_the_string % 2 == 0)
+	{
+		n = length_of_the_string / 2;
+		while (str[n] != '\0')
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	
+	else if (length_of_the_string % 2 != 0)
+	{
+		n = (length_of_the_string - 1) / 2;
+		n++;
+
+		while (*str != '\0')
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+
 	_putchar('\n');
 }	
