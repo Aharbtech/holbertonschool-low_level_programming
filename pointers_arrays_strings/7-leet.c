9ff04396244a1743c4a446aee2c0cf6d05dbe248
Aharbtech <9951@holbertonstudents.com>
2024-11-16 15:47:23 +0000

    L33t

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+char *leet(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char *l = "aAeEoOtTlL";
+	char *n = "4433007711";
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j <= 9; j++)
+		{	
+			if (s[i] == l[j])
+			{	
+				s[i] = n[j];
+			}
+		}
+	i++;
+	}
+
+	return (s);
+}
